bb7561778befb0f2c6408502f17fb71087787a19
KayvanShah1 <shahkayvan28@gmail.com>
2021-06-01 16:55:53 +0530

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,31 +1,24 @@
-from typing import Optional
 from fastapi import FastAPI, Request
 
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from .models.item import Item
+from app.models.item import Item
+from app.routers import items
 
 
 app = FastAPI()
 
+# Static Files and Templates
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(directory="app/templates")
 
+# Routers
+app.include_router(items.router)
 
-@app.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+def landing_page(request: Request):
     data = {"page": "Home page"}
     return templates.TemplateResponse("index.html",{"request": request, "data": data})
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
---
+++ app/routers/items.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from fastapi import APIRouter
+
+from app.models.item import Item
+
+
+router = APIRouter(
+    prefix="/items",
+    tags=["items"],
+    responses={
+        404: {"description": "Not found"}
+    }
+)
+
+@router.get("/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@router.put("/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
